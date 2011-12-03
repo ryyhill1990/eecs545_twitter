b4144afa381316b61a2cda7cfb5aa97a25125151
Dolan Antenucci <antenucci.d@gmail.com>
2011-12-03 12:53:58 -0500

    updated to include august-data processing

--- preprocessing/hashtag_terms.py
+++ preprocessing/hashtag_terms.py
@@ -2,6 +2,7 @@
 import sys
 from nltk.tokenize import WhitespaceTokenizer
 
+BLOCKED_TAGS = ('#fb',)
 
 def main(argv):
     # looping over input
@@ -11,11 +12,13 @@ def main(argv):
         line = line.strip()
         date, user, post = line.split('\t', 2)
         
+        post = post.replace('#', ' #')
+        
         tokens = WhitespaceTokenizer().tokenize(post)
         for i in range(len(tokens)):
-            if tokens[i][0] == '#':
+            if tokens[i][0] == '#' and tokens[i] not in BLOCKED_TAGS:
                 for j in range(len(tokens)):
-                    if i != j:
+                    if i != j and tokens[j] not in BLOCKED_TAGS:
                         print "\t".join((tokens[i], tokens[j], date, user))
     
 if __name__ == '__main__':
